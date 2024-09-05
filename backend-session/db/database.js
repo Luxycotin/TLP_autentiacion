93bfaa560dbcadfa9027ad813c5c75d54664bb0e
Luca Espinoza <luca.g.esp2005@gmail.com>
2024-09-05 15:46:42 -0300

    Se empezó con la modularización

--- backend-session/db/database.js
+++ backend-session/db/database.js
@@ -1,25 +1,12 @@
-export const database = 
-    {
-        user: [
-            {
-                id: 1,
-                username: 'admin',
-                password: 'admin'
-            },
-            {
-                id: 2,
-                username: 'user',
-                password: 'user'
-            },
-            {
-                id: 3,
-                username: 'guest',
-                password: 'guest'
-            },
-            {
-                id: 4,
-                username: 'guest2',
-                password: 'guest2'
-            }
-        ]
-    }
+import {} from "mysql/promise"
+const rekest = async (req, res)=>{
+    const pedilo = rekest ({
+        host: "hostlocal",
+        user: "root ",
+        password: "",
+        database: "db_system"
+    });
+    return pedilo
+}
+const pool = await rekest()
+export { pool }
